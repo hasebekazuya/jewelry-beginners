9b051e4ebbdfb09ac8716a9f7c4b68ce4129d1db
hasebekazuya <tabi85nack5877@icloud.com>
2023-02-26 09:17:29 +0000

    search完了

--- app/Http/Controllers/Admin/JewelryController.php
+++ app/Http/Controllers/Admin/JewelryController.php
@@ -120,21 +120,55 @@ class JewelryController extends Controller
         // }
         
         //$posts = Gem::where('gem_name','like', 'ア%')->where('gem_name','like', 'イ%')->orWhere('gem_name','like', 'ウ%')->orWhere('gem_name','like', 'エ%')->orWhere('gem_name','like', 'オ%')->get();
-$posts =  Gem::where(function($query){
-$query->orWhere('gem_name', 'like', 'ア%')
-        ->orWhere('gem_name', 'like', 'イ%')
-        ->orWhere('gem_name', 'like', 'ウ%')
-        ->orWhere('gem_name', 'like', 'エ%')
-        ->orWhere('gem_name', 'like', 'オ%');
-        })->get();
-$posts2=Gem::where(function($query){
-$query->orWhere('gem_name', 'like', 'ア%')
-      ->orWhere('gem_name', 'like', 'イ%')
-      ->orWhere('gem_name', 'like', 'ウ%')
-      ->orWhere('gem_name', 'like', 'エ%')
-      ->orWhere('gem_name', 'like', 'オ%');
-    })->get();
+        $posts =  Gem::where(function($query){
+        $query->orWhere('gem_name', 'like', 'ア%')
+              ->orWhere('gem_name', 'like', 'イ%')
+              ->orWhere('gem_name', 'like', 'ウ%')
+              ->orWhere('gem_name', 'like', 'エ%')
+              ->orWhere('gem_name', 'like', 'オ%');
+              })->get();
+        $posts2 = Gem::where(function($query){
+        $query->orWhere('gem_name', 'like', 'カ%')
+              ->orWhere('gem_name', 'like', 'キ%')
+              ->orWhere('gem_name', 'like', 'ク%')
+              ->orWhere('gem_name', 'like', 'ケ%')
+              ->orWhere('gem_name', 'like', 'コ%');
+            })->get();
+        $posts3 = Gem::where(function($query){
+        $query->orWhere('gem_name', 'like', 'サ%')
+              ->orWhere('gem_name', 'like', 'シ%')
+              ->orWhere('gem_name', 'like', 'ス%')
+              ->orWhere('gem_name', 'like', 'セ%')
+              ->orWhere('gem_name', 'like', 'ソ%');
+            })->get(); 
+        $posts4 = Gem::where(function($query){
+        $query->orWhere('gem_name', 'like', 'タ%')
+              ->orWhere('gem_name', 'like', 'チ%')
+              ->orWhere('gem_name', 'like', 'ツ%')
+              ->orWhere('gem_name', 'like', 'テ%')
+              ->orWhere('gem_name', 'like', 'ト%');
+            })->get();
+        $posts5 = Gem::where(function($query){
+        $query->orWhere('gem_name', 'like', 'ナ%')
+              ->orWhere('gem_name', 'like', 'ニ%')
+              ->orWhere('gem_name', 'like', 'ヌ%')
+              ->orWhere('gem_name', 'like', 'ネ%')
+              ->orWhere('gem_name', 'like', 'ノ%')
+              ->orWhere('gem_name', 'like', 'ハ%')
+              ->orWhere('gem_name', 'like', 'マ%')
+              ->orWhere('gem_name', 'like', 'ヤ%')
+              ->orWhere('gem_name', 'like', 'ラ%')
+              ->orWhere('gem_name', 'like', 'ワ%');
+            })->get();
+        $posts6 = Gem::where(function($query){
+        $query->orWhere('gem_name', 'like', 'ラ%')
+              ->orWhere('gem_name', 'like', 'リ%')
+              ->orWhere('gem_name', 'like', 'ル%')
+              ->orWhere('gem_name', 'like', 'レ%')
+              ->orWhere('gem_name', 'like', 'ロ%');
+            })->get();    
         //$posts = Gem::whereIn('gem_name', 'like', ['ア%','イ%','ウ%', 'エ%', 'オ%'])->get();
-        return  view('admin.jewelry.search', ['posts' => $posts, 'posts2'=> $posts2]);
+        
+        return  view('admin.jewelry.search', ['posts' => $posts, 'posts2'=> $posts2,'posts3' => $posts3, 'posts4'=> $posts4, 'posts5' => $posts5, 'posts6'=> $posts6]);
     }
 }
--- resources/views/admin/jewelry/search.blade.php
+++ resources/views/admin/jewelry/search.blade.php
@@ -29,10 +29,40 @@
                             </li>
                         </ul>
                     @endforeach
-                     <div class="col-md-8 mx-auto">
-                
                     <h4>カ行</h4>
                     @foreach($posts2 as $gem)
+                        <ul>
+                            <li>
+                                <a href="{{ route('admin.jewelry.show', ['id' => $gem->id]) }}" target="_blank">{{$gem->gem_name}}</a>
+                            </li>
+                        </ul>
+                    @endforeach
+                    <h4>サ行</h4>
+                    @foreach($posts3 as $gem)
+                        <ul>
+                            <li>
+                                <a href="{{ route('admin.jewelry.show', ['id' => $gem->id]) }}" target="_blank">{{$gem->gem_name}}</a>
+                            </li>
+                        </ul>
+                    @endforeach
+                    <h4>タ行</h4>
+                    @foreach($posts4 as $gem)
+                        <ul>
+                            <li>
+                                <a href="{{ route('admin.jewelry.show', ['id' => $gem->id]) }}" target="_blank">{{$gem->gem_name}}</a>
+                            </li>
+                        </ul>
+                    @endforeach
+                    <h4>マ・ハ行</h4>
+                    @foreach($posts5 as $gem)
+                        <ul>
+                            <li>
+                                <a href="{{ route('admin.jewelry.show', ['id' => $gem->id]) }}" target="_blank">{{$gem->gem_name}}</a>
+                            </li>
+                        </ul>
+                    @endforeach
+                    <h4>ラ行</h4>
+                    @foreach($posts6 as $gem)
                         <ul>
                             <li>
                                 <a href="{{ route('admin.jewelry.show', ['id' => $gem->id]) }}" target="_blank">{{$gem->gem_name}}</a>
